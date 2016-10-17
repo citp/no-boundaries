27d52319c8bf137c9501d78a50e115872b479fac
englehardt <englehardt@gmail.com>
2016-10-17 14:54:44 -0400

    Adding Gunes' public key and making it possible to specify key on download utility

--- identity_project/download_crawl.sh
+++ identity_project/download_crawl.sh
@@ -4,22 +4,23 @@
 # Exit if we hit any errors
 set -e
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: ./download_crawl.sh <server_ip_or_hostname> <output_folder>"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: ./download_crawl.sh <keyname> <server_ip_or_hostname> <output_folder>"
     exit 1
 fi
 
-host=ubuntu@$1
-data_dir=$2
+keyname=$1
+host=ubuntu@$2
+data_dir=$3
 
 echo Checking if crawls are running on $1
-if ssh -i ~/.ssh/crawler_access -q $host [[ -f /tmp/crawl_running ]]; then
+if ssh -i ~/.ssh/$keyname -q $host [[ -f /tmp/crawl_running ]]; then
     echo "Can't archive while crawls are running on the machine, exiting."
     exit 1
 fi
 
 printf '\nCrawls to archive: \n'
-crawl_dirs=( $(ssh -i ~/.ssh/crawler_access $host 'ls -1 --ignore "*.tar.bz2" | grep 2016') )
+crawl_dirs=( $(ssh -i ~/.ssh/$keyname $host 'ls -1 --ignore "*.tar.bz2" | grep 2016') )
 for dir in ${crawl_dirs[@]}; do
     echo '  *'$dir
 done
@@ -27,21 +28,21 @@ done
 for dir in ${crawl_dirs[@]}; do
     printf '\n===============================================\n'
     printf 'Processing: '$dir'\n'
-    crawl_db=( $(ssh -i ~/.ssh/crawler_access $host 'ls -1 '$dir'/*.sqlite') )
+    crawl_db=( $(ssh -i ~/.ssh/$keyname $host 'ls -1 '$dir'/*.sqlite') )
 
     printf '\n Moving crontab log into data directory: '$dir
-    ssh -i ~/.ssh/crawler_access $host 'cp crontab.log '$dir
+    ssh -i ~/.ssh/$keyname $host 'cp crontab.log '$dir
 
     printf '\n Taking md5sum of '$crawl_db'...'
-    remote_md5sum=( $(ssh -i ~/.ssh/crawler_access $host 'md5sum '$crawl_db ) )
+    remote_md5sum=( $(ssh -i ~/.ssh/$keyname $host 'md5sum '$crawl_db ) )
     printf $remote_md5sum'\n'
 
     compressed=$dir'.tar.bz2'
     printf '\n Compressing '$dir' to '$compressed' on remote server\n'
-    ssh -i ~/.ssh/crawler_access $host 'tar -c '$dir' | pbzip2 -c > '$compressed
+    ssh -i ~/.ssh/$keyname $host 'tar -c '$dir' | pbzip2 -c > '$compressed
 
     printf '\n Copying '$compressed' from remote to '$data_dir' on local\n'
-    scp -i ~/.ssh/crawler_access $host:'/home/ubuntu/'$compressed $data_dir
+    scp -i ~/.ssh/$keyname $host:'/home/ubuntu/'$compressed $data_dir
 
     printf '\n Decompressing local copy\n'
     local_archive=$data_dir$compressed
@@ -58,8 +59,8 @@ for dir in ${crawl_dirs[@]}; do
     fi
 
     #printf '\n Deleting remote archive and crawl database \n'
-    #ssh -i ~/.ssh/crawler_access $host 'rm -rf '$dir
-    #ssh -i ~/.ssh/crawler_access $host 'rm '$compressed
+    #ssh -i ~/.ssh/$keyname $host 'rm -rf '$dir
+    #ssh -i ~/.ssh/$keyname $host 'rm '$compressed
 
     printf '\n Deleting local decompressed archive\n'
     echo $data_dir$dir
