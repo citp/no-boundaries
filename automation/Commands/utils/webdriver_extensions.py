5ff8ad9b03b89a4f7b549ac0443500190417eee9
englehardt <englehardt@gmail.com>
2017-03-30 14:11:06 -0400

    PEP8 Fixes for webdriver_extensions.py

--- automation/Commands/utils/webdriver_extensions.py
+++ automation/Commands/utils/webdriver_extensions.py
@@ -15,39 +15,49 @@ import XPathUtil
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-#### Basic functions
+# Basic functions ########
 def scroll_down(driver):
     at_bottom = False
     while random.random() > .20 and not at_bottom:
         k = str(10 + int(200*random.random()))
         driver.execute_script("window.scrollBy(0,"+k+")")
-        at_bottom = driver.execute_script("return (((window.scrollY + window.innerHeight ) +100 > document.body.clientHeight ))")
+        at_bottom = driver.execute_script("""
+            return (((window.scrollY + window.innerHeight ) + 100 > document.body.clientHeight ))""") # noqa
         time.sleep(0.5 + random.random())
+
 
 def scroll_to_bottom(driver):
     try:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
     except WebDriverException:
         pass
 
+
 def is_loaded(webdriver):
-    return (webdriver.execute_script("return document.readyState") == "complete")
+    return (
+        webdriver.execute_script("return document.readyState") == "complete")
+
 
 def wait_until_loaded(webdriver, timeout, period=0.25):
     mustend = time.time() + timeout
     while time.time() < mustend:
-        if is_loaded(webdriver): return True
+        if is_loaded(webdriver):
+            return True
         time.sleep(period)
     return False
+
 
 def get_intra_links(webdriver, url):
     ps1 = du.get_ps_plus_1(url)
     links = filter(lambda x: (x.get_attribute("href") and
-                              du.get_ps_plus_1(urljoin(url, x.get_attribute("href"))) == ps1),
+                              du.get_ps_plus_1(urljoin(
+                                  url, x.get_attribute("href"))) == ps1),
                    webdriver.find_elements_by_tag_name("a"))
     return links
 
-##### Search/Block Functions
+
+# Search/Block Functions
 # locator_type: a text representation of the standard
 # webdriver.find_element_by_* functions. You can either
 # import selenium.webdriver.common.by.By and use By.LINK_TEXT, etc.
@@ -56,11 +66,14 @@ def get_intra_links(webdriver, url):
 # By.CSS_SELECTOR is 'css selector'
 # By.NAME is 'name' ... and so on
 # locator: string that you are looking for
-def wait_and_find(driver, locator_type, locator, timeout=3, check_iframes=True):
+
+
+def wait_and_find(driver, locator_type, locator,
+                  timeout=3, check_iframes=True):
     if is_found(driver, locator_type, locator, timeout):
         return driver.find_element(locator_type, locator)
     else:
-        if check_iframes: #this may return the browser with an iframe active
+        if check_iframes:  # this may return the browser with an iframe active
             driver.switch_to_default_content()
             iframes = driver.find_elements_by_tag_name('iframe')
 
@@ -70,9 +83,10 @@ def wait_and_find(driver, locator_type, locator, timeout=3, check_iframes=True):
                 if is_found(driver, locator_type, locator, timeout=0):
                     return driver.find_element(locator_type, locator)
 
-            #If we get here, search also fails in iframes
+            # If we get here, search also fails in iframes
             driver.switch_to_default_content()
-        raise NoSuchElementException, "Element not found during wait_and_find"
+        raise NoSuchElementException("Element not found during wait_and_find")
+
 
 def is_found(driver, locator_type, locator, timeout=3):
     try:
@@ -82,6 +96,7 @@ def is_found(driver, locator_type, locator, timeout=3):
     except TimeoutException:
         return False
 
+
 def is_visible(driver, locator_type, locator, timeout=3):
     try:
         w = WebDriverWait(driver, timeout)
@@ -89,6 +104,7 @@ def is_visible(driver, locator_type, locator, timeout=3):
         return True
     except TimeoutException:
         return False
+
 
 def title_is(driver, title, timeout=3):
     try:
@@ -98,6 +114,7 @@ def title_is(driver, title, timeout=3):
     except TimeoutException:
         return False
 
+
 def title_contains(driver, title, timeout=3):
     try:
         w = WebDriverWait(driver, timeout)
@@ -106,13 +123,16 @@ def title_contains(driver, title, timeout=3):
     except TimeoutException:
         return False
 
-#Selenium requires an element to be visible and enabled to be
-#clickable. We extend that to require it to have a tag capable
-#of containing a link. NOTE: doesn't work 100%
-def is_clickable(driver, full_xpath, xpath, timeout = 1):
+
+def is_clickable(driver, full_xpath, xpath, timeout=1):
+    """
+    Selenium requires an element to be visible and enabled to be
+    clickable. We extend that to require it to have a tag capable
+    of containing a link. NOTE: doesn't work 100%
+    """
     try:
         w = WebDriverWait(driver, timeout)
-        w.until(EC.element_to_be_clickable(('xpath',xpath)))
+        w.until(EC.element_to_be_clickable(('xpath', xpath)))
         return XPathUtil.is_clickable(full_xpath)
     except (TimeoutException, ElementNotVisibleException):
         return False
@@ -137,14 +157,15 @@ def str_element(element):
 
     Return empty string if the element is not active anymore."""
     try:
-        return '<%s type="%s" name="%s" value="%s" placeholder="%s" ...> x:%s y:%s w:%d h:%d' %\
-            (element.tag_name, get_element_type(element),
-             element.get_attribute("name"), element.get_attribute("value"),
-             get_placeholder_text(element),
-             element.location["x"], element.location["y"],
-             element.size["width"], element.size["height"])
+        out = '<%s type="%s" name="%s" value="%s" placeholder="%s" ...> x:%s y:%s w:%d h:%d' % ( # noqa
+            element.tag_name, get_element_type(element),
+            element.get_attribute("name"), element.get_attribute("value"),
+            get_placeholder_text(element),
+            element.location["x"], element.location["y"],
+            element.size["width"], element.size["height"])
     except:
-        return ""
+        out = ""
+    return out
 
 
 def is_text_or_email_input(element):
@@ -195,36 +216,3 @@ def iter_frames(driver):
         driver.switch_to_default_content()
         yield iframe
     driver.switch_to_default_content()
-
-
-#TODO Update this. No direct access to DB right now
-'''
-#get and set xpaths into xpath database
-def get_xpath(driver, url, name):
-cur = self.db.cursor()
-cur.execute("SELECT xpath FROM xpath WHERE url = ? AND name = ?",(url, name))
-response = cur.fetchone()
-if response == None:
-return None
-else:
-return response[0]
-
-def set_xpath(driver, url, name, xpath, absolute_xpath = None):
-cur = self.db.cursor()
-if self.mp_lock is not None:
-self.mp_lock.acquire()
-cur.execute("UPDATE xpath SET xpath = ?, absolute_xpath = ? \
-WHERE url = ? AND name = ?", (xpath, absolute_xpath, url, name))
-if cur.rowcount == 0: #occurs when record does not already exist
-cur.execute("INSERT INTO xpath (name, url, xpath, absolute_xpath) VALUES (?,?,?,?)",
-(name, url, xpath, absolute_xpath))
-self.db.commit()
-if self.mp_lock is not None:
-self.mp_lock.release()
-return cur.lastrowid
-'''
-
-#Click an xpath using javascript -- not working correctly
-#gets around visibility requirements of selenium.
-#def click_xpath(driver, xpath):
-# driver.execute_script('$(document.evaluate('+xpath+', document, null, 9, null).singleNodeValue).click();')
