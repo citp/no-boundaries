affcdc0e839c103c99860e1083185228aa20de28
englehardt <englehardt@gmail.com>
2017-04-25 19:29:06 -0400

    Removing unneeded files

--- test/selenium_utils.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium import webdriver
-import os
-
-
-def get_webdriver():
-    current_dir = os.path.dirname(__file__)
-    fb = FirefoxBinary(os.path.join(current_dir, '../firefox-bin/firefox'))
-    return webdriver.Firefox(firefox_binary=fb)
