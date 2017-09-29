6af650439bccb508a2555fe22b04c181b53cf992
Gunes Acar <gunes.acar@esat.kuleuven.be>
2017-09-28 21:24:53 -0400

    Add missing Python packages for Travis tests.

--- install-analysis.sh
+++ install-analysis.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-sudo apt-get install pbzip2
-
-sudo pip install jupyter pandas pycrypto hackercodecs pyblake2 sha3 \
+sudo pip install pandas pycrypto hackercodecs pyblake2 sha3 \
   mmhash base58 cookies jsbeautifier python-whois
+
+#  No need to install pbzip2 and jupyter on Travis
+if [ "$TRAVIS" != "true" ]; then
+	sudo apt-get install pbzip2
+	sudo pip install jupyter
+fi
