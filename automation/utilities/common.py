88225bf4d38729ecab02369be2a339077cbc8f04
Gunes Acar <gunes.acar@esat.kuleuven.be>
2018-06-14 18:19:11 -0400

    Move DOM spoofing bait to common.py.

--- automation/utilities/common.py
+++ automation/utilities/common.py
@@ -2,3 +2,20 @@ FORM_FILL_EMAIL = "randomtestuser.1234@gmail.com"
 FORM_FILL_PASSWORD = "_pa$$word123_"
 FORM_SUBMISSION_MARK = "__FORM_SUBMISSION__"
 FORM_FILLING_MARK = "__FORM_FILLING__"
+
+
+FORM_FILL_CHECKOUT = {
+    'email': FORM_FILL_EMAIL,
+    'emailC': FORM_FILL_EMAIL,
+    'name': 'Random User',
+    'ship-address': '123 Not A Real Street',
+    'ship-city': 'New York',
+    'ship-state': 'NY',
+    'ship-zip': '10001',
+    'ship-country': 'USA',
+    'ccname': 'Random User',
+    'cardnumber': '5105105105105100',
+    'cvc': '123',
+    'cc-exp': '08/2019',
+    'phone': '123-555-0100'
+}
